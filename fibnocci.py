606bc64a8bdd7d4c2d0db76e3972bf554db8290e
Guna-999 <67533404+Guna-999@users.noreply.github.com>
2020-07-04 11:05:54 +0530

    Update fibnocci.py

--- fibnocci.py
+++ fibnocci.py
@@ -34,3 +34,24 @@ def fib(n) :
             a = b
             b = c
             print(c)
+            
+            
+            
+            
+           
+        
+ 
+#is it fibbinocci num or not:
+import math
+def perfectsquare(x) :
+    s = int (math.sqrt(x))
+    return s*s==x
+n = int(input('enter the number:'))
+result1 = 5*(n*n)+ 4
+result2 = 5*(n*n)+ 4
+
+if perfectsquare(result1) or perfectsquare(result2):
+    print(n, 'is fibonacci number')
+else:
+    print(n, 'is not fibonacci number')
+        
